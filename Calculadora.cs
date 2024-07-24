08d4037f02d412ec1273ba2974e116a55ec445e5
JuliaPaulaMenegazzoSesi <159256141+JuliaPaulaMenegazzoSesi@users.noreply.github.com>
2024-07-24 09:52:17 -0300

    Create Calculadora.cs

---
+++ Calculadora.cs
@@ -0,0 +1,9 @@
+using System;
+
+int num1, num2, op, res;
+Console.WriteLine("Informe o primeiro número: ");
+num1 = Convert.ToInt32(Console.Read());
+Console.WriteLine("Informe o segundo número: ");
+num2 = Convert.ToInt32(Console.Read());
+Console.WriteLine("Infome a operação a ser realizada: ");
+op = Convert.ToInt32(Console.Read());
