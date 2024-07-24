4a3aeb4fe04d47e93c4151e3aeba8e48c5579999
JuliaPaulaMenegazzoSesi <159256141+JuliaPaulaMenegazzoSesi@users.noreply.github.com>
2024-07-24 10:01:32 -0300

    Update Calculadora.cs

--- Calculadora.cs
+++ Calculadora.cs
@@ -2,8 +2,33 @@ using System;
 
 int num1, num2, op, res;
 Console.WriteLine("Informe o primeiro número: ");
-num1 = Convert.ToInt32(Console.Read());
+num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Informe o segundo número: ");
-num2 = Convert.ToInt32(Console.Read());
-Console.WriteLine("Infome a operação a ser realizada: ");
-op = Convert.ToInt32(Console.Read());
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Infome a operação a ser realizada: \n" +
+                    "Adição = 1\n" +
+                    "Subtração = 2\n" +
+                    "Multiplicação = 3\n" +
+                    "Divisão = 4");
+op = Convert.ToInt32(Console.ReadLine());
+
+if (op == 1)
+{
+    res = num1 + num2;
+    Console.WriteLine(num1 + " + " + num2 + " = " + res);
+} else if (op == 2)
+{
+    res = num1 - num2;
+    Console.WriteLine(num1 + " - " + num2 + " = " + res);
+} else if (op == 3)
+{
+    res = num1 * num2;
+    Console.WriteLine(num1 + " * " + num2 + " = " + res);
+} else if (op == 4)
+{
+    res = num1 / num2;
+    Console.WriteLine(num1 + " / " + num2 + " = " + res);
+} else
+{
+    Console.WriteLine("Inválido");
+}
